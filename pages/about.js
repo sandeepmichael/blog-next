cb9783eda64c58851afdf7dc5ad2267f042b1705
Mahadari Sandeep <mahadarisandeep@gmail.com>
2022-04-21 20:19:27 +0530

    modified css

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
 import Layout from "../components/layout"
 import Image from 'next/image'
+import styles from '../styles/about.module.css'
 
 export default function AboutPage() {
   return (
     <Layout title='About Me'>
         <h1 className="text-5xl border-b-4 pb-5 font-bold ml-5">About</h1>
-        <div className="container mx-auto bg-white shadow-md rounded-lg px-10 py-6 mt-6">
+        <div className={styles.main}>
     
         <Image
     src="/images/profile.jpg" // Route of the image file
@@ -15,7 +16,7 @@ export default function AboutPage() {
     className="borderCircle"
   />
           
-            <p>
+            <p className={styles.para}>
                 <span className="font-bold">Hello I'm M Sandeep.I'm  a full stack developer..</span>
             </p>
 
